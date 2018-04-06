31443cc3c07ee173163b9b22fbe94a42f56cc8a5
RazorEdge347 <renem347@gmail.com>
2018-04-06 16:17:21 +0100

    Start of the tutorial

---
+++ 01_Tutorial/Tutorial_01/Source/Tutorial_01.Target.cs
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Tutorial_01Target : TargetRules
+{
+	public Tutorial_01Target(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+
+		ExtraModuleNames.AddRange( new string[] { "Tutorial_01" } );
+	}
+}
---
+++ 01_Tutorial/Tutorial_01/Source/Tutorial_01/Tutorial_01GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Tutorial_01GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TUTORIAL_01_API ATutorial_01GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
